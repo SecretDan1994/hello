ee630a36ea18a76f494dda10d7d2f3debbe314f7
SecretDan1994 <ec2-user@ip-172-31-85-10.ec2.internal>
2019-11-22 14:03:03 -0500

    Added test.go

---
+++ test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Job struct {
+    Title string
+    Description string
+}
+
+func (j *Job) SetTitle(title string) {
+    j.Title = title
+}
+
+func (j *Job) SetDescription(description string) {
+    j.Description = description
+}
+
+func (j *Job) GetTitle() string {
+    return j.Title
+}
+
+func (j *Job) GetDescription() string {
+    return j.Description
+}
+
+func main() {
+    j := Job{}
+    j.SetTitle("Programmer")
+    j.SetDescription("Someone who programs..")
+    fmt.Printf("My job is %s with description %s\n", j.GetTitle(), j.GetDescription())
+}
